40a26d3b82beca48b467d2f1747a32eae36499b1
sadmanryanriad <sadmanryanriad@gmail.com>
2023-12-02 19:45:15 +0600

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+Server side employee management
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
