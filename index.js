4e833b4f2a06e7dcc4ad000e5edd0c7cfdedc6ea
sadmanryanriad <sadmanryanriad@gmail.com>
2023-12-03 15:22:49 +0600

    store user data

--- index.js
+++ index.js
@@ -21,6 +21,46 @@ app.use(express.json());
 app.use(cookieParser());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ni8nft9.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // MongoDB collections
+    const userCollection = client.db("Assignment-12").collection("users");
+
+    // store users data
+    app.post('/users', async(req,res)=>{
+        const user = req.body;
+        //check if the email already exists
+        const query = { email: user?.email};
+        const existingUser = await userCollection.findOne(query);
+        if(existingUser){
+            return res.send({message: "user already exists", insertedId: null})
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    //remove this before deploy
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
